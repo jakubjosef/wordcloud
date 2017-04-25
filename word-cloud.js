9a7f68ee5df040dfc1d71ab05c6a61f45cf29ab0
Jakub Josef <jakub.josef@gmail.com>
2017-04-25 17:39:41 +0200

    Implemented chart updating

--- word-cloud.js
+++ word-cloud.js
@@ -1,26 +1,12 @@
-var API_URL="tags.json";
+var API_URL="tags.json"
+ ,  REFRESH_INTERVAL=10;
 
 var visualization = function(visualization){
-    var max, fontSize, layout, svg,  vis, w, h
+    var data, layout, svg,  vis, w, h
         , fill = d3.scale.category20b();
-    visualization.init = function(tags){
+    visualization.init = function(){
         w = window.innerWidth;
         h = window.innerHeight;
-        // sort tags by weight
-        tags.sort(function(a,b){
-           return a.weight - b.weight;
-        });
-
-        var tagSize = 15
-         , tagValue = 0;
-        tags.forEach(function(val){
-          if(tagValue != val.weight){
-            tagValue = val.weight;
-            tagSize = tagSize + 5
-          }
-          val.size = tagSize;
-        });
-
         layout = d3.layout.cloud()
                 .timeInterval(Infinity)
                 .size([w, h])
@@ -31,18 +17,15 @@ var visualization = function(visualization){
                     return d.text;
                 })
                 .on("end", visualization.draw);
-        if(vis){
-            vis.remove();
-        }
         if(svg){
             svg.remove();
         }
         svg = d3.select("#vis").append("svg")
         vis = svg.append("g");
-        visualization.update(tags);
+        visualization.update();
 
         window.onresize = function(event) {
-            visualization.update(tags);
+            visualization.update();
         };
     };
 
@@ -74,7 +57,7 @@ var visualization = function(visualization){
                 .style("opacity", 1);
         text.style("font-family", function(d) {
             return d.font;
-        })
+                })
                 .style("fill", function(d) {
                     return fill(d.text.toLowerCase());
                 })
@@ -83,7 +66,7 @@ var visualization = function(visualization){
                 });
     };
 
-    visualization.update = function(tags) {
+    visualization.update = function() {
         w = window.innerWidth;
         h = window.innerHeight;
         svg.attr("width", w)
@@ -91,16 +74,40 @@ var visualization = function(visualization){
         vis.attr("transform", "translate(" + [w >> 1, h >> 1] + ")");
         layout.font('impact').spiral('archimedean');
         fontSize = d3.scale['log']().range([1, 100]);
-        if (tags.length){
-            fontSize.domain([+tags[tags.length - 1].size || 1, +tags[0].size]);
+        if (data.length){
+            fontSize.domain([+data[data.length - 1].size || 1, +data[0].size]);
         }
-        layout.stop().words(tags).start();
+        layout.stop().words(data).start();
+    };
+
+    visualization.fetchData = function(makeUpdate){
+        $.getJSON(API_URL).fail( function(d, textStatus, error) {
+            console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+        }).done(function(res){
+            data = res;
+            // sort data by weight
+            data.sort(function(a,b){
+               return a.weight - b.weight;
+            });
+            // patch data weights
+            var tagSize = 15
+             , tagValue = 0;
+            data.forEach(function(val){
+              if(tagValue != val.weight){
+                tagValue = val.weight;
+                tagSize = tagSize + 5
+              }
+              val.size = tagSize;
+            });
+            if(typeof makeUpdate === 'boolean' && makeUpdate){
+                visualization.update();
+            }else{
+                visualization.init();
+            }
+        });
     };
     return visualization;
 }(visualization || {});
 
-$.getJSON(API_URL).fail( function(d, textStatus, error) {
-            console.error("getJSON failed, status: " + textStatus + ", error: "+error)
-        }).done(function(tags){
-       visualization.init(tags);
-    });
+$(visualization.fetchData);
+setInterval(visualization.fetchData.bind(null, true), REFRESH_INTERVAL*1000)
